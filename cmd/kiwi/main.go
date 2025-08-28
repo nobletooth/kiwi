f07dca1d4ddeb0c12e381ceab469005effd6ab77
Parsa Saberi <saberi@yektanet.ir>
2025-08-28 15:11:46 +0330

    add kiwi server skeleton

--- cmd/kiwi/main.go
+++ cmd/kiwi/main.go
@@ -1,16 +1,33 @@
+// Spins up the kiwi server, compatible w/ the Redis protocol.
+
 package main
 
 import (
-	"fmt"
-	"strings"
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/nobletooth/kiwi/pkg/port"
+	"github.com/nobletooth/kiwi/pkg/storage"
 )
 
 func main() {
-	lsm := newLSMTree()
-	val, err := lsm.Get(strings.Repeat("d", kSize))
-	fmt.Println(val, err)
+	ctx, cancel := context.WithCancel(context.Background())
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
 
-	//lsm.Set(strings.Repeat("c", kSize), strings.Repeat("b", vSize))
-	//lsm.Set(strings.Repeat("d", kSize), strings.Repeat("b", vSize))
-	//lsm.Flush()
+	go func() { // Listen for OS interrupts in the background.
+		select {
+		case sig := <-signals:
+			slog.Info("Received termination signal, cancelling server context.", "signal", sig)
+			cancel()
+		}
+	}()
+
+	store := storage.NewInMemoryKeyValueHolder()
+	if err := port.RunRedisServer(ctx, store); err != nil {
+		slog.Error("Kiwi server stopped.", "err", err)
+		os.Exit(1)
+	}
 }
